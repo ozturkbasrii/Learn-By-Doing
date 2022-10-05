1066d482ffcf169242a7a1849c4dcca5ec248fbf
ozturkbasrii <ozturkbasrii@gmail.com>
2022-10-05 09:41:43 +0300

    Initial Commit

---
+++ Learn By Doing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learn By Doing
+//
+//  Created by Hasan Basri Öztürk on 5.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Hasan Basri Öztürk on 5.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
